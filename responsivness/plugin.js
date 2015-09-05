3cbce56bc7968ec36dea62d2e3c913a17abaffd5
Kevin Gautreau <contact@kgaut.net>
2015-09-05 10:38:43 +0200

    coding standards

--- responsivness/plugin.js
+++ responsivness/plugin.js
@@ -1,23 +1,23 @@
 (function () {
-    CKEDITOR.plugins.add('responsivness', {
-        icon: 'responsivness',
-        init: function (editor) {
+  CKEDITOR.plugins.add('responsivness', {
+    icon: 'responsivness',
+    init: function (editor) {
 
-            // Register the toolbar buttons.
-            editor.ui.addButton('responsivness', {
-                label: Drupal.t('Insert a responsive area'),
-                icon: this.path + 'images/responsivness.png',
-                command: 'beResponsive'
-            });
+      // Register the toolbar buttons.
+      editor.ui.addButton('responsivness', {
+        label: Drupal.t('Insert a responsive area'),
+        icon: this.path + 'images/responsivness.png',
+        command: 'beResponsive'
+      });
 
-            var cssPath = this.path + 'responsivness.css';
+      var cssPath = this.path + 'responsivness.css';
 
-            editor.on('instanceReady', function () {
-                this.document.appendStyleSheet(cssPath);
-            });
-            editor.addCommand('beResponsive',new CKEDITOR.dialogCommand('beResponsive'));
+      editor.on('instanceReady', function () {
+        this.document.appendStyleSheet(cssPath);
+      });
+      editor.addCommand('beResponsive', new CKEDITOR.dialogCommand('beResponsive'));
 
-            CKEDITOR.dialog.add('beResponsive', this.path + 'dialogs/beResponsive.js');
-        }
-    });
+      CKEDITOR.dialog.add('beResponsive', this.path + 'dialogs/beResponsive.js');
+    }
+  });
 })();
