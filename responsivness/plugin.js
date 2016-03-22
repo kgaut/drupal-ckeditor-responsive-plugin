4c01fba39ba0bdf6005718885c856c8367b5349b
Kevin Gautreau <contact@kgaut.net>
2016-03-22 19:03:45 +0100

    Specify allowed class in ckeditor

--- responsivness/plugin.js
+++ responsivness/plugin.js
@@ -21,7 +21,9 @@
       editor.on('instanceReady', function () {
         this.document.appendStyleSheet(cssPath);
       });
-      editor.addCommand('beResponsive', new CKEDITOR.dialogCommand('beResponsive', {allowedContent: 'div(*)'}));
+      editor.addCommand('beResponsive', new CKEDITOR.dialogCommand('beResponsive', {
+        allowedContent: ' div(ckeditor-col-container,first-col,last-col,onecol,twocol,threecol,fourcol,fivecol,sixcol,sevencol,eightcol,ninecol,tencol,elevencol,twelvecol,grid-1,grid-2,grid-3,grid-4,grid-5,grid-6,grid-7,grid-8,grid-9,grid-10,grid-11,grid-12)'
+      }));
 
       CKEDITOR.dialog.add('beResponsive', this.path + 'dialogs/beResponsive.js');
     }
